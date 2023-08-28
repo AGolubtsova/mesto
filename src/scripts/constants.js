c4ad969a84f84f71e7df562ca58d2a2e323a4559
Anna Golubtsova <marickam@yandex.ru>
2023-08-28 21:12:35 +0300

    webpack build implemented

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,6 +1,6 @@
 export const initialCards = [
     {
-      name: 'Архыз',
+      name: 'Архыз!!',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
