0416dbf35e9c8208f8113fc0437187d4f2fd5344
Anna Golubtsova <marickam@yandex.ru>
2023-08-30 15:19:03 +0300

    add class PopupWithImage

---
+++ src/scripts/PopupWithImage.js
@@ -0,0 +1,23 @@
+import Popup from './Popup.js';
+
+class PopupWithImage extends Popup {
+  // Наследует от Popup и принимает в конструктор селектор popup
+  constructor(popupSelector) {
+    super(popupSelector);
+    // this._popupItem находится в родительском классе
+    this._popupCaption = this._popupItem.querySelector('.popup__caption');
+    this._popupImage = this._popupItem.querySelector('.popup__image');
+  }
+
+  // Метод перезаписывает родительский метод open
+  open(name, link) {
+    // Вставляем в popup картинку с src изображения и подписью к картинке
+    this._popupCaption.textContent = name;
+    this._popupImage.src = link;
+    this._popupImage.alt = name;
+    super.open();
+  }
+}
+
+//Экспортируем класс в index.js
+export default PopupWithImage;
