581461b099b9cba4ce1be78a4e5b2cbf399c0ece
Anna Golubtsova <marickam@yandex.ru>
2023-08-30 14:34:39 +0300

    add class section and class popup

---
+++ src/scripts/Section.js
@@ -0,0 +1,18 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+      this._renderedItems = items;
+      this._renderer = renderer;
+      this._container = document.querySelector(containerSelector);
+  }
+
+    // Метод принимает DOM-элемент и добавляет его в контейнер
+  addItem(cardElement) {
+    this._container.prepend(cardElement);
+  }
+  // Метод отрисовки всех элементов
+  renderItems() {
+    this._renderedItems.forEach(item => {
+      this._renderer(item);
+    });
+  }
+}
